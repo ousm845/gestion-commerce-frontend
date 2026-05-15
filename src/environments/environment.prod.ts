bee62d9c22c655acc46bc9c4c1846f9dbb2e3ea9
assane <votre.email@example.com>
2026-05-15 21:36:58 +0000

    fix: update production API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://azoudev07.pythonanywhere.com',
   appName: 'SyliDigit - Gestion Intégrée',
   version: '1.0.0'
 };
