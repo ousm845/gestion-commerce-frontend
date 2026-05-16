36050a9c47f2f75a891463efc7dee023d45d351b
assane <votre.email@example.com>
2026-05-16 14:10:13 +0000

    fix: use production URL in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api',
+  apiUrl: 'https://azoudev07.pythonanywhere.com/api',
   appName: 'SyliDigit - Gestion Intégrée',
   version: '1.0.0'
 };
